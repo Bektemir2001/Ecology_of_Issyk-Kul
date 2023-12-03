ed4a4cd0c0dbefaea88a57733c00a7ff7478f852
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2023-12-03 18:41:46 +0600

    add admin

--- routes/admin.php
+++ routes/admin.php
@@ -1,4 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\IndexController;
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => 'admin'], function (){
+    Route::get('/', [IndexController::class, 'index'])->name('admin.index');
+});
