2ad2ad7f200dad013b25ae8048defcc9ca121a04
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2024-03-21 17:40:50 +0600

    report

--- app/Repositories/ReportRepository.php
+++ app/Repositories/ReportRepository.php
@@ -8,12 +8,12 @@ class ReportRepository
 {
     public function fields()
     {
-        $fields = Field::select('field', 'model', 'table_field');
+        $fields = Field::select('field', 'model', 'table_field')->get();
         $fields->each(function ($value){
             if($value->model)
             {
                 $model = app()->getNamespace() . 'Models\\' . $value->model;
-                $value->children = $model::all();
+                $value->children = $model::select('id', 'name')->get();
             }
         });
         return $fields;
