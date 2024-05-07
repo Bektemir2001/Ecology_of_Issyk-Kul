b3d9f5bc315a9c45a271ba810b34dd94b436ff86
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2024-05-07 11:29:19 +0600

    save chnages

--- routes/auth.php
+++ routes/auth.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\AdminAuthController;
 use App\Http\Controllers\Auth\OperatorAuthController;
 use App\Http\Controllers\Auth\UserAuthController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/operator/login', [OperatorAuthController::class, 'loginIndex'])->name('auth.operator.login.index');
@@ -12,6 +13,10 @@ Route::post('/operator/login', [OperatorAuthController::class, 'login'])->name('
 Route::get('/admin/login', [AdminAuthController::class, 'loginIndex'])->name('auth.admin.login.index');
 Route::post('/admin/login', [AdminAuthController::class, 'login'])->name('auth.admin.login');
 
+Route::post('/logout', function (){
+    Auth::logout();
+    return redirect()->route('auth.operator.login');
+});
 
 Route::get('/unauthorized', function (){
     return response(['message' => 'unauthorized'])->setStatusCode(401);
