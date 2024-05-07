f630ec323127669422dc0629d852ab0439daae61
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2024-05-07 17:34:21 +0600

    save changes

--- app/Services/FormulaService.php
+++ app/Services/FormulaService.php
@@ -9,32 +9,74 @@ class FormulaService
 {
     public function formula1($chlA)
     {
-        return 2.22 + 2.54 * log10($chlA);
+
+        try {
+            return 2.22 + 2.54 * log10($chlA);
+        }
+        catch (DivisionByZeroError $e) {
+            throw new Exception($e);
+        }
     }
     public function formula2($sd)
     {
-        return 5.10 + 2.60 * log10((1 / $sd) - (1 / 40));
+
+        try {
+            return 5.10 + 2.60 * log10((1 / $sd) - (1 / 40));
+        }
+        catch (DivisionByZeroError $e) {
+            throw new Exception($e);
+        }
     }
     public function formula3($tp)
     {
-        return 0.218 + 2.92 * log10($tp);
+
+        try {
+            return 0.218 + 2.92 * log10($tp);
+        }
+        catch (DivisionByZeroError $e) {
+            throw new Exception($e);
+        }
     }
     public function formula4($tn)
     {
-        return -3.61 + 3.01 * log10($tn);
+
+        try {
+            return -3.61 + 3.01 * log10($tn);
+        }
+        catch (DivisionByZeroError $e) {
+            throw new Exception($e);
+        }
     }
     public function formula5($item)
     {
-        return 10 * (6 - ((2.04 - 0.68 * log($item)/log(2))));
+
+        try {
+            return 10 * (6 - ((2.04 - 0.68 * log($item)/log(2))));
+        }
+        catch (DivisionByZeroError $e) {
+            throw new Exception($e);
+        }
     }
     public function formula6($item)
     {
-        return 10 * (6 - ((log(48/$item)/log(2))));
+
+        try {
+            return 10 * (6 - ((log(48/$item)/log(2))));
+        }
+        catch (DivisionByZeroError $e) {
+            throw new Exception($e);
+        }
     }
 
     public function formula7($item)
     {
-        return 10 * (6 - (log($item)/log(2)));
+
+        try {
+            return 10 * (6 - (log($item)/log(2)));
+        }
+        catch (DivisionByZeroError $e) {
+            throw new Exception($e);
+        }
     }
     public function formula8($item)
     {
--- app/Services/TrophicStateIndexService.php
+++ app/Services/TrophicStateIndexService.php
@@ -65,10 +65,6 @@ class TrophicStateIndexService
             $data->each(function ($value) use ($pointElements){
                 $value->SD_TSI = $this->formulaService->formula7($value->transparency);
                 $value->elements = $pointElements->where('point_id', '=', $value->point_id)->each(function ($item){
-                    if($item->item == 0)
-                    {
-                        dd($item);
-                    }
                     $item->tsi = call_user_func([$this->formulaService, $item->element->TSI_formula], $item->item);
 
                 });
@@ -131,6 +127,10 @@ class TrophicStateIndexService
                     }
                 }
 
+            }
+            if(count($elements) != 4)
+            {
+                return [[], []];
             }
             return [$elements, $control_points];
         }
