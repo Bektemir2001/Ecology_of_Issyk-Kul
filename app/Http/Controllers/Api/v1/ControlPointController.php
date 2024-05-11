e98c354fdb917633ee697134289ad64df458205f
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2024-05-11 18:05:41 +0600

    save changes

--- app/Http/Controllers/Api/v1/ControlPointController.php
+++ app/Http/Controllers/Api/v1/ControlPointController.php
@@ -8,19 +8,20 @@ use App\Http\Resources\ControlPointResource;
 use App\Models\ControlPoint;
 use App\Models\Element;
 use App\Models\Field;
+use App\Models\MajorIon;
 use Carbon\Carbon;
-
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ControlPointController extends Controller
 {
-    public function getAll()
+    public function getAll(): AnonymousResourceCollection
     {
         return ControlPointResource::collection(ControlPoint::all()->each(function ($controlPoint) {
             $controlPoint->setAttribute('color', '#000');
         }));
     }
 
-    public function getWithPDK(ReportRequest $request)
+    public function getWithPDK(ReportRequest $request): AnonymousResourceCollection
     {
         $data = $request->validated();
         $control_points = ControlPoint::all();
@@ -31,6 +32,14 @@ class ControlPointController extends Controller
             if($data['table_field'] == 'elements')
             {
                 $control_points = $this->elements($control_points, $data['children'], $year);
+            }
+            else if($data['table_field'] == 'major_ions')
+            {
+                $control_points = $this->ions($control_points, $data['children'], $year);
+            }
+            else if($data['table_field'] == 'organic_substances')
+            {
+                $control_points = $this->organics($control_points, $data['children'], $year);
             }
             return ControlPointResource::collection($control_points);
         }
@@ -49,7 +58,7 @@ class ControlPointController extends Controller
         return ControlPointResource::collection($control_points);
     }
 
-    public function getColor($item, $pdk)
+    public function getColor($item, $pdk): string
     {
         $colors = ['#000', '#32CD32', '#FFFF00', '#FF0000'];
         if($item == null || $pdk == null || $item == 0)
@@ -86,13 +95,35 @@ class ControlPointController extends Controller
         });
         return $control_points;
     }
-    public function ions()
+    public function ions($control_points, $ion_id, $year)
     {
-
+        $pdk = MajorIon::query()->where('id', $ion_id)->first()->pdk_up;
+        $control_points->each(function ($item) use ($year, $pdk, $ion_id) {
+            $points = $item->points;
+            $points = $points->filter(function ($point) use ($year, $ion_id) {
+                $date = Carbon::createFromFormat('Y-m-d', $point->date);
+                $point->ion_item = $point->pointIons->where('ion_id', $ion_id)->avg('item');
+                return $year == strval($date->year);
+            });
+            $value = $points->avg('ion_item');
+            $item->setAttribute('color', $this->getColor($value, $pdk));
+        });
+        return $control_points;
     }
 
-    public function organics()
+    public function organics($control_points, $organic_id, $year)
     {
-
+        $pdk = MajorIon::query()->where('id', $organic_id)->first()->pdk_up;
+        $control_points->each(function ($item) use ($year, $pdk, $organic_id) {
+            $points = $item->points;
+            $points = $points->filter(function ($point) use ($year, $organic_id) {
+                $date = Carbon::createFromFormat('Y-m-d', $point->date);
+                $point->organic_item = $point->pointOrganics->where('organic_substance_id', $organic_id)->avg('item');
+                return $year == strval($date->year);
+            });
+            $value = $points->avg('organic_item');
+            $item->setAttribute('color', $this->getColor($value, $pdk));
+        });
+        return $control_points;
     }
 }
