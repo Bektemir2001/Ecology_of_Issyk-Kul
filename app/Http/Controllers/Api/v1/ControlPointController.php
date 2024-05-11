bad852da60f777303503691732f11cab47e8098d
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2024-05-11 16:00:22 +0600

    save changes

--- app/Http/Controllers/Api/v1/ControlPointController.php
+++ app/Http/Controllers/Api/v1/ControlPointController.php
@@ -25,13 +25,15 @@ class ControlPointController extends Controller
 
         $result = ControlPoint::all();
         $year = $data['year'];
-        $result->each(function ($item) use ($year) {
+        $table_field = $data["table_field"];
+        $result->each(function ($item) use ($year, $table_field) {
             $points = $item->points;
             $points = $points->filter(function ($point) use ($year) {
                 $date = Carbon::createFromFormat('Y-m-d', $point->date);
                 return $year == strval($date->year);
             });
-            dd($points);
+            $item = $points->avg($table_field);
+            dd($item);
             $item->points = $points;
         });
         dd($result);
