ef564e520273a958ef450886193db23477f239bc
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2024-05-11 16:30:11 +0600

    save changes

--- app/Http/Controllers/Api/v1/ControlPointController.php
+++ app/Http/Controllers/Api/v1/ControlPointController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ReportRequest;
 use App\Http\Resources\ControlPointResource;
 use App\Models\ControlPoint;
+use App\Models\Field;
 use Carbon\Carbon;
 
 
@@ -26,17 +27,39 @@ class ControlPointController extends Controller
         $result = ControlPoint::all();
         $year = $data['year'];
         $table_field = $data["table_field"];
-        $result->each(function ($item) use ($year, $table_field) {
+        $pdk = Field::query()->where('table_field', $table_field)->first();
+        $result->each(function ($item) use ($year, $table_field, $pdk) {
             $points = $item->points;
             $points = $points->filter(function ($point) use ($year) {
                 $date = Carbon::createFromFormat('Y-m-d', $point->date);
                 return $year == strval($date->year);
             });
             $item = $points->avg($table_field);
-            dd($item);
-            $item->points = $points;
+            $item->color = $this->getColor($item, $pdk->pdk_up);
         });
         dd($result);
+    }
+
+    public function getColor($item, $pdk)
+    {
+        $colors = ['#000', '#32CD32', '#FFFF00', '#FF0000'];
+        if($item == null || $pdk == null || $item == 0)
+        {
+            return $colors[0];
+        }
+        $avg = (($item - $pdk)/$pdk) * 100;
+        if($avg <= 10 and $avg >= -10)
+        {
+            return $colors[1];
+        }
+        elseif($avg > 10)
+        {
+            return $colors[3];
+        }
+        else
+        {
+            return $colors[2];
+        }
     }
 
 
