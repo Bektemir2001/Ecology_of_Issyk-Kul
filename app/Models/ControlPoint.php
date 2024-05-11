f1c6dabed526919c59ce2fb06c7797d84f2791d8
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2024-05-11 16:34:56 +0600

    save changes

--- app/Models/ControlPoint.php
+++ app/Models/ControlPoint.php
@@ -16,7 +16,7 @@ class ControlPoint extends Model
     protected $guarded = false;
 
 
-    public string $color = "#000";
+    public $color = "#000";
     public function district(): BelongsTo
     {
         return $this->belongsTo(District::class);
